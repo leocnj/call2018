5009452bfdc4bf67ec2f97c6fdc1a5e008362e95
Lei Chen <lei.chen@liulishuo.com>
2018-01-23 14:59:56 -0500

    Cleaned train_model_v2.py
    - removed un-used stuffs
    - updated result display
    - also added kNN and LR model.

--- src/train_model_v2.py
+++ src/train_model_v2.py
@@ -1,69 +1,21 @@
 import pickle
 
-import numpy as np
-from hyperopt import fmin, tpe, Trials
-from hyperopt import hp
-from hyperopt import space_eval
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score
 from xgboost.sklearn import XGBClassifier
-from sklearn.model_selection import KFold, StratifiedKFold
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import StratifiedKFold
 from sklearn.model_selection import cross_val_score
-
-from mlens.ensemble import BlendEnsemble
 
 from utils import *
 import argparse
 
 """
-- Support RF, XGBoost, and SVM
-- Use **kwargs dict to pass parameters
-- Use ML ens to stack models
-- Use hyperopt for more convenient space definition
 
 """
 SEED = 1
-
-
-def define_model(**params):
-    if params['model'] == 'RF':
-        n_estimators = params['n_estimators']
-        max_features = params['max_features']
-        model = RandomForestClassifier(n_estimators=n_estimators,
-                                       max_features=max_features)
-    elif params['model'] == 'XGB':
-        n_estimators = params['n_estimators']
-        max_depth = params['max_depth']
-        model = XGBClassifier(n_estimators=n_estimators,
-                              max_depth=max_depth)
-    elif params['model'] == 'SVM':
-        C = params['C']
-        kernel = params['kernel']
-        model = SVC(C=C, kernel=kernel, probability=True)
-    else:
-        pass
-    return model
-
-@timeit
-def find_optm_params(objective, space, max_evals=20):
-    trials = Trials()
-    best = fmin(objective,
-                space,
-                algo=tpe.suggest,
-                max_evals=max_evals,
-                trials=trials)
-    best_params = space_eval(space, best)
-    return best_params
-
-
-def stack_models(base_estimators, type):
-    blender = BlendEnsemble()
-    blender.add(base_estimators, proba=True)
-    blender.add_meta(LogisticRegression())
-    train_eval(blender, type)
-
 
 
 if __name__ == '__main__':
@@ -110,24 +62,13 @@ if __name__ == '__main__':
         return 1 - cv_score.mean()
 
 
-    def train_eval(best_model):
-        best_model.fit(lang_train_X, lang_train_y)
-        y_true, y_pred = lang_test_y, best_model.predict(lang_test_X)
-        print('acc: %1.3f' % accuracy_score(y_true, y_pred))
-
-
-    def eval_best_model(optm_params):
-        best_model = define_model(**optm_params)
-        train_eval(best_model)
-
-
     def cv_acc(model):
         cv_score = cross_val_score(model,
                                    lang_train_X, lang_train_y,
                                    cv=shuffle_inEval,
                                    scoring='accuracy', n_jobs=-1)
-        print(cv_score)
-        print(np.median(cv_score))
+        acc_test = accuracy_score(lang_test_y, model.predict(lang_test_X))
+        print('Acc mean on train: {:2.4f}\tAcc on test: {:2.4f}'.format(cv_score.mean(), acc_test))
 
 
     # Parse args
@@ -145,19 +86,23 @@ if __name__ == '__main__':
         model = SVC(probability=True, random_state=SEED)
     elif model_type == 'XGB':
         model = XGBClassifier()
+    elif model_type == 'kNN':
+        model = KNeighborsClassifier()
+    elif model_type == 'LR':
+        model = LogisticRegression(random_state=SEED)
     else:
         print('wrong model type {}'.format(model_type))
 
-    cv_acc(model)
-    # XGB needs very small thres, e.g., 0.185
+    model.fit(lang_train_X, lang_train_y)
+    cv_acc(model)  # show Acc in training and test
+
     for thres in [0.05, 0.10, 0.15, 0.20, 0.25, 0.30, 0.35, 0.40, 0.45, 0.50, 0.55, 0.60]:
         Ds = cross_val_D(model, train_X,  train_y, cv=shuffle_inEval, THRES=thres)
-        print('------------------------------------------------')
-        print('thres: {} mean of D {}'.format(thres, Ds.mean()))
         # D on the REAL test set.
         D_test = get_D_on_proba(model.predict_proba(lang_test_X),
                                 test_y, THRES=thres, print=False)
-        print('on test set final D: {}'.format(D_test))
+        print('------------------------------------------------')
+        print('thres: {}\tave D: {:2.4f}\t D on test: {:2.4f}'.format(thres, Ds.mean(), D_test))
 
     
 
