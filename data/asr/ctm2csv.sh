a5945c56b1ab6094845c5367530d934dbe35b296
Lei Chen <lei.chen@liulishuo.com>
2018-01-24 11:45:31 -0500

    Tidy bash

--- data/asr/ctm2csv.sh
+++ data/asr/ctm2csv.sh
@@ -9,18 +9,18 @@ python ../../src/parse_ctm.py --ctm raw/02-align-conf-20-0.5.ctm  >> 2018_train_
 python ../../src/parse_ctm.py --ctm raw/03-align-conf-20-0.5.ctm  >> 2018_train_asr.csv
 python ../../src/parse_ctm.py --ctm raw/04-align-conf-20-0.5.ctm  >> 2018_train_asr.csv
 
-#WERs are:
+# WERs are:
 #
-#Training set:
+# Training set:
 #
-#0) WER: 13.46
-#1) WER:  9.32
-#2) WER: 26.80
-#3)WER:  11.68
-#4) WER: 12.80
-#Weighted Average WER: 12.4
+# 0) WER: 13.46
+# 1) WER:  9.32
+# 2) WER: 26.80
+# 3) WER:  11.68
+# 4) WER: 12.80
+# Weighted Average WER: 12.4%
 #
-#Wer for test set is: 13.64%
+# WER for test set is: 13.64%
 
 # ST2 train 6698 utterances  ASR contains 6090
 # ST1 test   995 utterances  ASR contains 995
