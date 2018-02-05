58eb726d35405dc6801ec591118619696ddbc187
Lei Chen <lei.chen@liulishuo.com>
2018-02-05 10:47:18 -0500

    refactored runexp_pair.py
    - converted main body to one_expm(objs, model_type, shuffle, shuffle_inEval) func

--- src/runexp_pair.py
+++ src/runexp_pair.py
@@ -1,5 +1,3 @@
-import pickle
-
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
@@ -38,7 +36,7 @@ def prep_data(train_csv, test_csv):
     X = df_ta.iloc[:, 3:].values
     y = df_ta['language'].values
 
-    selector = RFECV(estimator=RandomForestClassifier(random_state=2018), cv=5, step=1, verbose=1, n_jobs=-1)
+    selector = RFECV(estimator=RandomForestClassifier(random_state=2018), cv=5, step=1, verbose=0, n_jobs=-1)
     selector.fit(X, y)
 
     feat_names = df_ta.columns[3:]
@@ -49,6 +47,13 @@ def prep_data(train_csv, test_csv):
 
     X_ta = get_langauge_X(df_ta, col_selected)
     X_ts = get_langauge_X(df_ts, col_selected)
+
+    # pre-processing X
+    scaler = StandardScaler()
+    scaler.fit(X_ta)
+    X_ta = scaler.transform(X_ta)
+    X_ts = scaler.transform(X_ts)
+
     y_l_ta = get_langauge_y(df_ta)
     y_m_ta = get_meaning_y(df_ta)
     y_l_ts = get_langauge_y(df_ts)
@@ -57,56 +62,17 @@ def prep_data(train_csv, test_csv):
     return [X_ta, y_l_ta, X_ts, y_l_ts, y_m_ta, y_m_ts]
 
 
-if __name__ == '__main__':
-
-    # Parse args
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-t', '--model_type', type=str, help='model type',
-                        required=True)
-    parser.add_argument('--train', type=str, help='train csv', required=True)
-    parser.add_argument('--test', type=str, help='test csv', required=True)
-    args = parser.parse_args()
-    model_type = args.model_type
-    train_csv = args.train
-    test_csv = args.test
-
-    objs = prep_data(train_csv, test_csv)
+def one_expm(objs, model_type, shuffle, shuffle_inEval):
     lang_train_X = objs[0]
     lang_train_y = objs[1]
     lang_test_X = objs[2]
     lang_test_y = objs[3]
     meaning_train_y = objs[4]
     meaning_test_y = objs[5]
-
     train_y = np.column_stack((meaning_train_y, lang_train_y))
     test_y = np.column_stack((meaning_test_y, lang_test_y))
 
-    # skip lots of sklearn depreciation warnings
-    import warnings
-    warnings.filterwarnings("ignore")
-
-    # logger = get_logger(__name__, simple=True)
-
-    # to use same CV data splitting
-    shuffle = StratifiedKFold(n_splits=10, random_state=SEED)
-    shuffle_inEval = StratifiedKFold(n_splits=10, random_state=SEED + 1024)
-
-    def cv_acc(model):
-        # for train CV
-        cv_score = cross_val_score(model,
-                                   lang_train_X, lang_train_y,
-                                   cv=shuffle_inEval,
-                                   scoring='accuracy', n_jobs=-1)
-        # for test
-        model.fit(lang_train_X, lang_train_y)
-        acc_test = accuracy_score(lang_test_y, model.predict(lang_test_X))
-        print('Acc mean on train: {:2.4f}\tAcc on test: {:2.4f}'.format(cv_score.mean(), acc_test))
-
-
-
     print('model type: {}'.format(model_type))
-
-
     if model_type == 'RF':
         model = RandomForestClassifier(random_state=SEED)
     elif model_type == 'SVC':
@@ -119,18 +85,22 @@ if __name__ == '__main__':
         model = LogisticRegression(random_state=SEED)
     else:
         print('wrong model type {}'.format(model_type))
-
+    cv_score = cross_val_score(model,
+                               lang_train_X, lang_train_y,
+                               cv=shuffle,
+                               scoring='accuracy', n_jobs=-1)
+    # for test
     model.fit(lang_train_X, lang_train_y)
-    cv_acc(model)  # show Acc in training and test
+    acc_test = accuracy_score(lang_test_y, model.predict(lang_test_X))
+    print('Acc mean on train: {:2.4f}\tAcc on test: {:2.4f}'.format(cv_score.mean(), acc_test))
 
-    # thres < 0.3 may cause iRj less than 25% and therefore fail in meeting challenge's requirement
-    # thres_lst = [0.20, 0.25, 0.30, 0.325, 0.350, 0.375, 0.40, 0.425, 0.45, 0.50]
     thres_lst = [0.20, 0.25, 0.30, 0.350, 0.40, 0.45, 0.50]
     for thres in thres_lst:
         print('---------------------------------------------------------------------------------------------')
-        Ds, ICRs, CRs = cross_val_D(model, lang_train_X,  train_y, cv=shuffle_inEval, THRES=thres)
+        Ds, ICRs, CRs = cross_val_D(model, lang_train_X, train_y, cv=shuffle_inEval, THRES=thres)
         # D on the REAL test set.
-        D_test, ICR_test, CR_test = get_D_on_proba(model.predict_proba(lang_test_X), test_y, THRES=thres, print=False, CR_adjust=False)
+        D_test, ICR_test, CR_test = get_D_on_proba(model.predict_proba(lang_test_X), test_y, THRES=thres, print=False,
+                                                   CR_adjust=False)
         print('Thres:{}\tCV D:{:2.4f} ICR:{:2.4f} CR:{:2.4f}\tTest D:{:2.4f} ICR:{:2.4f} CR:{:2.4f}'.format(thres,
                                                                                                             Ds.mean(),
                                                                                                             ICRs.mean(),
@@ -138,10 +108,29 @@ if __name__ == '__main__':
                                                                                                             D_test,
                                                                                                             ICR_test,
                                                                                                             CR_test))
-    
 
+if __name__ == '__main__':
 
-    # with open('../data/processed/numpy/year17_models.pkl', 'wb') as pf:
-    #     pickle.dump([optm_params_RF_l, optm_params_RF_m,
-    #                  optm_params_XGB_l, optm_params_XGB_m,
-    #                  optm_params_SVM_l, optm_params_SVM_m], pf)
+    # skip lots of sklearn depreciation warnings
+    import warnings
+    warnings.filterwarnings("ignore")
+    # logger = get_logger(__name__, simple=True)
+
+    # Parse args
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-t', '--model_type', type=str, help='model type',
+                        required=True)
+    parser.add_argument('--train', type=str, help='train csv', required=True)
+    parser.add_argument('--test', type=str, help='test csv', required=True)
+    args = parser.parse_args()
+
+    model_type = args.model_type
+    train_csv = args.train
+    test_csv = args.test
+    objs = prep_data(train_csv, test_csv)
+
+    # to use same CV data splitting
+    shuffle = StratifiedKFold(n_splits=10, random_state=SEED)
+    shuffle_inEval = StratifiedKFold(n_splits=10, random_state=SEED + 1024)
+
+    one_expm(objs, model_type, shuffle, shuffle_inEval)
