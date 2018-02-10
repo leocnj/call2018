7dd354c1dbcbe49e0c97e65810a4f605383c1019
Lei Chen <lei.chen@liulishuo.com>
2018-02-09 20:05:22 -0500

    Updated runexp_eval.py
    - removed unused imports
    - pred_fname now contains model_file ID

--- src/runexp_eval.py
+++ src/runexp_eval.py
@@ -1,16 +1,3 @@
-from sklearn.ensemble import RandomForestClassifier, VotingClassifier
-from sklearn.linear_model import LogisticRegression
-from sklearn.svm import SVC
-from xgboost.sklearn import XGBClassifier
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.model_selection import StratifiedKFold
-from sklearn.model_selection import cross_val_score
-from sklearn.feature_selection import RFECV, RFE
-from sklearn.preprocessing.data import StandardScaler
-from sklearn.pipeline import make_pipeline
-
-from tpot import TPOTClassifier
-from utils import *
 import argparse
 import pandas as pd
 import pickle
@@ -37,12 +24,12 @@ def prep_test(test_csv, pipe):
 
 
 import os
-def pred_fname(test_csv):
+def pred_fname(model_file, test_csv):
+    model_id = os.path.splitext(os.path.basename(model_file))[0]
     csv_id = os.path.splitext(os.path.basename(test_csv))[0]
-    model_file = '../ml_exp/preds/' + csv_id + '.csv'
+    model_file = '../ml_exp/preds/' + model_id + '_' + csv_id + '.csv'
     print(model_file)
     return model_file
-
 
 if __name__ == '__main__':
 
@@ -65,7 +52,7 @@ if __name__ == '__main__':
     with open(model_file, 'rb') as pf:
          pipe, ml_model = pickle.load(pf)
 
-    pred_file = pred_fname(test_csv)
+    pred_file = pred_fname(model_file, test_csv)
     X_ts = prep_test(test_csv, pipe)
     probs = ml_model.predict_proba(X_ts)
     print(probs)
