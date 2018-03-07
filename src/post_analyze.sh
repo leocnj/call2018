f29c290b26f852e2633c16f3105077920c03b644
Lei Chen <lei.chen@liulishuo.com>
2018-03-06 19:59:41 -0500

    - added prep_ml_data_trans.py for features w/ transcriptions - added one more section in post_analyze.sh for trans task.

--- src/post_analyze.sh
+++ src/post_analyze.sh
@@ -27,3 +27,129 @@ if [ $RUN_PY -eq 1 ]; then
   python runexp_eval.py --model_file $MODEL_FILE --test ${TS_18} --year 2018_text --thres $THRES
   python comp_result.py $CSV
 fi
+
+
+# 4 A, text, 0.5
+EXPM="Expm 4: 2018 A, Ensemble, Text, 0.5"
+echo $EXPM
+RUN_PY=0
+
+THRES=0.5
+TASK=text
+TASK2=text
+TA_17=${DATA}/y17_train_${TASK}.csv
+TA_18_A=${DATA}/y18_train_A_${TASK}.csv
+TA_18_B=${DATA}/y18_train_B_${TASK}.csv
+TA_18_C=${DATA}/y18_train_C_${TASK}.csv
+TA_18="$TA_18_A"
+TS_18=${DATA}/y18_test_${TASK2}.csv
+
+MODEL_FILE=${PIPE}/y18_train_A_${TASK}-${MODEL}.pkl
+CSV=${PRED}/y18_train_A_${TASK}-${MODEL}_y18_test_${TASK2}_t${THRES}.csv
+if [ $RUN_PY -eq 1 ]; then
+  python runexp_train.py --train ${TA_18}  -t ${MODEL} --fit
+  python runexp_eval.py --model_file $MODEL_FILE --test ${TS_18} --year 2018_text --thres $THRES
+  python comp_result.py $CSV
+fi
+
+# 8 2017 + A, asr-asr, 0.5
+EXPM="Expm 8: 2017 + 2018A, Ensemble, asr-asr, 0.5"
+echo $EXPM
+RUN_PY=0
+
+THRES=0.35
+TASK=asr
+TASK2=asr
+TA_17=${DATA}/y17_train_${TASK}.csv
+TA_18_A=${DATA}/y18_train_A_${TASK}.csv
+TA_18_B=${DATA}/y18_train_B_${TASK}.csv
+TA_18_C=${DATA}/y18_train_C_${TASK}.csv
+TA_18="$TA_18_A"
+TS_18=${DATA}/y18_test_${TASK2}.csv
+
+MODEL_FILE=${PIPE}/y18_train_A_${TASK}-y17_train_${TASK}-${MODEL}.pkl
+CSV=${PRED}/y18_train_A_${TASK}-y17_train_${TASK}-${MODEL}_y18_test_${TASK2}_t${THRES}.csv
+if [ $RUN_PY -eq 1 ]; then
+  python runexp_train.py --train ${TA_18} ${TA_17} -t ${MODEL} --fit
+  python runexp_eval.py --model_file $MODEL_FILE --test ${TS_18} --year 2018_asr --thres $THRES
+  python comp_result.py $CSV
+fi
+
+
+# 10 2017 + A, asr-2best, 0.35
+# 11 2017 + A, asr-2best, 0.5
+EXPM="Expm 10/11: 2017 + 2018A, Ensemble, asr-2best, 0.35"
+echo $EXPM
+RUN_PY=0
+
+THRES=0.55 # 0.5
+TASK=asr
+TASK2=2best
+TA_17=${DATA}/y17_train_${TASK}.csv
+TA_18_A=${DATA}/y18_train_A_${TASK}.csv
+TA_18_B=${DATA}/y18_train_B_${TASK}.csv
+TA_18_C=${DATA}/y18_train_C_${TASK}.csv
+TA_18="$TA_18_A"
+TS_18=${DATA}/y18_test_${TASK2}.csv
+
+MODEL_FILE=${PIPE}/y18_train_A_${TASK}-y17_train_${TASK}-${MODEL}.pkl
+CSV=${PRED}/y18_train_A_${TASK}-y17_train_${TASK}-${MODEL}_y18_test_${TASK2}_t${THRES}.csv
+if [ $RUN_PY -eq 1 ]; then
+  python runexp_train.py --train ${TA_18} ${TA_17} -t ${MODEL} --fit
+  python runexp_eval.py --model_file $MODEL_FILE --test ${TS_18} --year 2018_asr --thres $THRES
+  python comp_result.py $CSV
+fi
+
+# 1 2017 + A, text, 0.35
+EXPM="Expm 1: 2017 + 2018A, Ensemble, text, 0.35"
+#
+# Confirmed we can re-produce our submission result
+# the F-1 provided by the CALL challenge seems a micro-version value
+#
+echo $EXPM
+RUN_PY=0
+
+THRES=0.3
+TASK=text
+TASK2=text
+TA_17=${DATA}/y17_train_${TASK}.csv
+TA_18_A=${DATA}/y18_train_A_${TASK}.csv
+TA_18_B=${DATA}/y18_train_B_${TASK}.csv
+TA_18_C=${DATA}/y18_train_C_${TASK}.csv
+TA_18="$TA_18_A"
+TS_18=${DATA}/y18_test_${TASK2}.csv
+
+MODEL_FILE=${PIPE}/y18_train_A_${TASK}-y17_train_${TASK}-${MODEL}.pkl
+CSV=${PRED}/y18_train_A_${TASK}-y17_train_${TASK}-${MODEL}_y18_test_${TASK2}_t${THRES}.csv
+if [ $RUN_PY -eq 1 ]; then
+  python runexp_train.py --train ${TA_18} ${TA_17} -t ${MODEL} --fit
+  python runexp_eval.py --model_file $MODEL_FILE --test ${TS_18} --year 2018_text --thres $THRES
+  python comp_result.py $CSV
+fi
+
+# Tran 2017 + A, tran, 0.35
+EXPM="Expm of using trans: 2017 + 2018A, Ensemble, text, 0.35"
+#
+# Confirmed we can re-produce our submission result
+# the F-1 provided by the CALL challenge seems a micro-version value
+#
+echo $EXPM
+RUN_PY=1
+
+THRES=0.4
+TASK=tran
+TASK2=tran
+TA_17=${DATA}/y17_train_${TASK}.csv
+TA_18_A=${DATA}/y18_train_A_${TASK}.csv
+TA_18_B=${DATA}/y18_train_B_${TASK}.csv
+TA_18_C=${DATA}/y18_train_C_${TASK}.csv
+TA_18="$TA_18_A"
+TS_18=${DATA}/y18_test_${TASK2}.csv
+
+MODEL_FILE=${PIPE}/y18_train_A_${TASK}-y17_train_${TASK}-${MODEL}.pkl
+CSV=${PRED}/y18_train_A_${TASK}-y17_train_${TASK}-${MODEL}_y18_test_${TASK2}_t${THRES}.csv
+if [ $RUN_PY -eq 1 ]; then
+  python runexp_train.py --train ${TA_18} ${TA_17} -t ${MODEL} --fit
+  python runexp_eval.py --model_file $MODEL_FILE --test ${TS_18} --year 2018_text --thres $THRES
+  python comp_result.py $CSV
+fi
---
+++ src/prep_ml_data_trans.py
@@ -0,0 +1,136 @@
+import pickle
+
+import pandas as pd
+
+
+def recode_df(df):
+    recode = lambda x: 1 if x == 'correct' else 0
+    df['language'] = df['language'].apply(recode)
+    df['meaning'] = df['meaning'].apply(recode)
+    return df
+
+
+def get_17df():
+    with open('../data/processed/data.pkl', 'rb') as pf:
+        objs = pickle.load(pf)
+
+    df_train = objs[1]  # using RecResult
+    df_test = objs[2]  # using RecResult
+
+    df_train = recode_df(df_train)
+    df_test = recode_df(df_test)
+
+    print('2017 DF')
+    print('train: {} test: {}'.format(df_train.shape, df_test.shape))
+    return (df_train, df_test)
+
+
+def get_18df():
+    with open('../data/processed/data.pkl', 'rb') as pf:
+        objs = pickle.load(pf)
+
+    df_A = objs[3]  # using RecResult
+    df_B = objs[4]  # using RecResult
+    df_C = objs[5]
+
+    df_A = recode_df(df_A)
+    df_B = recode_df(df_B)
+    df_C = recode_df(df_C)
+
+    print('2018 DF')
+    for df_ in [df_A, df_B, df_C]:
+        print('shape: {}'.format(df_.shape))
+    return (df_A, df_B, df_C)
+
+
+def get_18test():
+    # 2018 test
+    spreadsheet_in = '../data/texttask_trainData/scst2_testDataText.csv'
+    df_18_test = pd.read_csv(spreadsheet_in, sep='\t', encoding="utf-8", na_filter=False)
+    print('2018 test\nshape: {}'.format(df_18_test.shape))
+    return df_18_test
+
+
+def load_huy(csv_file):
+    df = pd.read_csv(csv_file, sep='\t')
+    df.rename(columns={'ID': 'Id'}, inplace=True)
+    df.drop(columns=['CLASS'], inplace=True)
+    print('Huy features: {}'.format(df.shape))
+    # Id plus 43 columns
+    col_huy = ["Id", "ppl-ref", "ppl-ref_pos", "ppl-ref_prod", "ppl-ref_dep", "ppl-prompt", "ppl-prompt_pos",
+               "ppl-correct", "ppl-correct_pos", "ppl-correct_prod", "ppl-correct_dep", "ppl-ge", "ppl-ge_pos",
+               "ppl-incorrect", "ppl-incorrect_pos", "ppl-incorrect_prod", "ppl-incorrect_dep", "maxsim_15_skip",
+               "maxsim_30_skip", "maxsim_50_skip", "maxsim_15_cbw", "maxsim_30_cbw", "maxsim_50_cbw", "lda_sim-min",
+               "ngram_match", "ngram_match-lem", "ngram_unseen-1", "ngram_unseen-2", "ngram_unseen-3", "error_count",
+               "parse_score-ratio", "length_ratio", "length_01", "length_unknown", "length_unknown-ratio",
+               "length_sounds", "length_sounds-ratio", "prompt_missing", "prompt_missing-pct", "prompt_DT", "prompt_IN",
+               "prompt_MD", "prompt_NN", "prompt_VB"]
+    df = df[col_huy]
+    print('reduced to {}'.format(df.shape))
+    return df
+
+
+def gen_ml_csv(df_main, grmerr_csv, huy_csv, ml_csv):
+    """
+
+    :param df_main:
+    :param grmerr_csv:
+    :param huy_csv:
+    :param ml_csv:
+    :return:
+    """
+    print('ml_csv: {}'.format(ml_csv))
+    df_grmerr = pd.read_csv(grmerr_csv)
+    df_huy = load_huy(huy_csv)
+
+    # 2018 test has no L and M columns
+    if {'language', 'meaning'}.issubset(df_main.columns):
+        df_ml = df_main[['Id', 'language', 'meaning']]
+    else:
+        df_ml = df_main[['Id']]
+
+    # df_ml = pd.merge(df_ml, df_grmerr, on='Id',
+    #                  how='left')  # grmerr may miss some Ids due to ASR null outputs. use left to keep all Ids.
+    df_ml = pd.merge(df_ml, df_huy, on='Id', how='left')
+    df_ml.fillna(0, inplace=True)
+    df_ml.to_csv(ml_csv, index=False)
+
+
+if __name__ == '__main__':
+    y17_train, y17_test = get_17df()
+    y18_train_A, y18_train_B, y18_train_C = get_18df()
+    y18_test = get_18test()
+
+    # ------------------------- TRAN task -------------------------------------------------
+    # 2017 train text
+    gen_ml_csv(y17_train,
+               '../data/processed/df17_train_grmerror.csv',
+               '../data/processed/Huy/v8_Trs/textProcessing_trainingKaldi_features.csv',
+               '../ml_exp/inputs/y17_train_tran.csv')
+    # # 2017 test text
+    # gen_ml_csv(y17_test,
+    #            '../data/processed/df17_test_grmerror.csv',
+    #            '../data/processed/Huy/v8_Trs/textProcessing_testKaldi_annotated_features.csv',
+    #            '../ml_exp/inputs/y17_test_tran.csv')
+
+    # text
+    # 2018 train A
+    gen_ml_csv(y18_train_A,
+               '../data/processed/df18_A_train_grmerror.csv',
+               '../data/processed/Huy/v8_Trs/scst2_training_data_A_text_features.csv',
+               '../ml_exp/inputs/y18_train_A_tran.csv')
+    # 2018 train B
+    gen_ml_csv(y18_train_B,
+               '../data/processed/df18_B_train_grmerror.csv',
+               '../data/processed/Huy/v8_Trs/scst2_training_data_B_text_features.csv',
+               '../ml_exp/inputs/y18_train_B_tran.csv')
+    # 2018 train C
+    gen_ml_csv(y18_train_C,
+               '../data/processed/df18_C_train_grmerror.csv',
+               '../data/processed/Huy/v8_Trs/scst2_training_data_C_text_features.csv',
+               '../ml_exp/inputs/y18_train_C_tran.csv')
+    # 2018 test
+    gen_ml_csv(y18_test,
+               '../data/processed/df18_test_text_grmerror.csv',
+               '../data/processed/Huy/v8_Trs/testDataWithJudgements_features.csv',
+               '../ml_exp/inputs/y18_test_tran.csv')
